94398c5b5c6f9cc5617d8958661021426693f92e
scheruku <priyanka.cherukumilli@gmail.com>
2013-08-07 09:50:05 -0400

    test one
    first app

---
+++ README.md
@@ -0,0 +1,10 @@
+Search for Tweets using Twitter search/streaming api
+
+Requirements other than Code Iginter
+* create your own Twitter Application through https://dev.twitter.com/
+* https://github.com/ElliottLandsborough/Codeigniter-Twitter-Search-Library for Search library
+
+Step involved
+1. Integrate all the required files in your code igniter project
+2. Create your own twitter application .Get the authenticate tockens through the process and use them in config/twitter.php.
+
---
+++ Test/application/config/twitter.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['user'] = 'cpriya';
+$config['pass'] = 'ilovechang';
+
+// required only for api v1.1 search
+$config['consumer_token']  = 'tbqr5wgKvdhi1LqWTozcXA';
+$config['consumer_secret'] = 'RQCLy4Q4BtukPJ5I3r94fAfZVDMXjHgODvMvso';
+$config['access_token']    = '17540610-LszLdLOVwyFqLC92oyc5n1p3P02vlDxVwIGQCmTg';
+$config['access_secret']   = 'FZAWiJp9NljgdPRzvEnW83RdO00f0tm3j4nyiS8Syyg';
+
+
+/* End of file twitter.php */
+/* Location: ./application/config/twitter.php */
